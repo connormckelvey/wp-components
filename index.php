8b4eaf75fd884bd981523796695fc4d59bc102cf
Connor McKelvey <connormckelvey@gmail.com>
2016-04-11 18:08:20 -0600

    clean up

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once __DIR__ . '/lib/Templates/Index.php';
-
-$Index = new Index();
-$Index->display();
--- page.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once __DIR__ . '/lib/Templates/Page.php';
-
-$Page = new Page();
-$Page->display();
